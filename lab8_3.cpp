a111c934ce5ea8af2d24e749d70772c3b6d309c7
Fluke <adisorn.sjr@gmail.com>
2022-12-20 16:24:49 +0700

    completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	char y;
+	if(x > (char)64 and x < (char)91){
+		y = x-1;
+	}else{
+		y = '0';
+	}
+	if(x == 'A'){
+		y = 'Z';
+	}
+	return y;
 }
 
 int main(){
